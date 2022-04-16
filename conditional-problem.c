ac3c6060173398bab8daed2f5c2518f9a407fe86
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-04-16 23:04:48 +0600

    Added Problem Two

--- conditional-problem.c
+++ conditional-problem.c
@@ -12,4 +12,17 @@ int main(){
     } else{
         printf("Maximum = %d", num2);
     }; */
-};
+
+    /* Write a C program to find maximum between three numbers using ladder if else or nested if. How to find maximum or minimum between three numbers using if else in C programming. Logic to find maximum or minimum between three numbers in C program. */
+
+    int num1, num2, num3;
+    scanf("%d %d %d", &num1, &num2, &num3);
+    if(num1>num2 && num1>num3){
+        printf("Maximum is: %d", num1);
+    } else if(num2>num1 && num2>num3){
+        printf("Maximum is: %d", num2);
+    }else{
+        printf("Maximum is: %d", num3);
+    };
+    return 0;
+}
