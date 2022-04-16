707f861b79d7cb856f6b671b0d00845a9268dbaa
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-04-16 22:39:32 +0600

    Complete problem one

---
+++ conditional-problem.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    /* 
+    Write a C program to find maximum between two numbers using if else. C program to input two numbers from user and find maximum between two numbers using if else. How to find maximum or minimum between two numbers using if else in C programming.
+     */
+    /* int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    if(num1>num2){
+        printf("Maximum = %d", num1);
+    } else{
+        printf("Maximum = %d", num2);
+    }; */
+};
